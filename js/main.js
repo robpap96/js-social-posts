06b80257703323ac7e7f70e1076081a7b9d46e85
robpap96 <robertopapagni1996@outlook.it>
2022-11-08 18:37:51 +0100

    milestone 1 completata e iniziata milestone 2

---
+++ js/main.js
@@ -0,0 +1,99 @@
+"use strict";
+const posts = [
+    {
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 80,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=112",
+        "author": {
+            "name": "Sofia Perlari",
+            "image": "https://unsplash.it/300/300?image=10"
+        },
+        "likes": 120,
+        "created": "2021-09-03"
+    },
+    {
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=234",
+        "author": {
+            "name": "Chiara Passaro",
+            "image": "https://unsplash.it/300/300?image=20"
+        },
+        "likes": 78,
+        "created": "2021-05-15"
+    },
+    {
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=24",
+        "author": {
+            "name": "Luca Formicola",
+            "image": null
+        },
+        "likes": 56,
+        "created": "2021-04-03"
+    },
+    {
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=534",
+        "author": {
+            "name": "Alessandro Sainato",
+            "image": "https://unsplash.it/300/300?image=29"
+        },
+        "likes": 95,
+        "created": "2021-03-05"
+    }
+];
+
+const postContainer = document.querySelector(".post");
+
+for(let i=0; i<posts.length; i++){
+
+    const currentPost = posts[i];
+
+    const post = `
+    <div class="post__header post-meta post-meta__data">
+        <div class="post-meta__data">${currentPost.created}</div>
+        <div class="author-name post__text"> <span>${currentPost.author.name}</span> </div>
+        <div class="author-image profile-pic profile-pic-default post__text"><img src=${currentPost.author.image} alt=""></div>
+        <div class="post__text"> ${currentPost.content}</div>
+        <div class="post__image post__image ">
+            <img src=${currentPost.media} alt="">
+        </div>
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="${currentPost.id}">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${currentPost.likes}</b> persone
+            </div>
+        </div> 
+    </div>
+    `;
+    postContainer.innerHTML += post;
+}
+
+const likeButton = document.querySelector(".like-button");
+likeButton.addEventListener("click", function(){
+    const liked = document.querySelector(".js-like-button");
+    liked.classList.add("like-button--liked");
+    const likeCounter = document.querySelector(".js-likes-counter");
+    likeCounter.innerHTML = `${posts[id].likes+1}`;
+});
